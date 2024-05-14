b17852cbd0cb39254545611e94e5d231a44b81ff
yasartt <yasartatlicioglu@gmail.com>
2024-05-14 14:12:33 +0300

    Full template to fill

--- fatmod.c
+++ fatmod.c
@@ -1,140 +1,210 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 #include <fcntl.h>
-#include <unistd.h>
+#include <assert.h>
+#include <ctype.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <errno.h>
+#include <linux/msdos_fs.h>
 #include <stdint.h>
 
-#define SECTOR_SIZE 512
-#define CLUSTER_SIZE 1024 // 2 sectors per cluster
-#define ROOT_DIR_CLUSTER 2
-#define ENTRY_SIZE 32
-#define FAT_START_CLUSTER 32
-#define EOF_CLUSTER 0x0FFFFFF8
+#define FALSE 0
+#define TRUE 1
 
-typedef struct {
-    char name[11]; // 8 for name + 3 for extension
-    uint8_t attr;
-    uint8_t reserved[10];
-    uint16_t start; // starting cluster of the file
-    uint32_t size;  // size of the file
-} DirectoryEntry;
+#define SECTORSIZE 512   // bytes
+#define CLUSTERSIZE  1024  // bytes
 
-// Function Prototypes
+#define FAT_EOC 0x0FFFFFF8
+#define FAT_START_SECTOR 32  // Define the start sector of the FAT table
+
 int readsector(int fd, unsigned char *buf, unsigned int snum);
 int writesector(int fd, unsigned char *buf, unsigned int snum);
-void list_files(int fd);
-void read_file_ascii(int fd, const char* filename);
-void read_file_binary(int fd, const char* filename);
-uint32_t read_fat_entry(int fd, uint32_t cluster);
-off_t cluster_to_offset(uint32_t cluster);
+void list_root_directory(int fd);
+void display_file_ascii(int fd, const char *filename);
+void display_file_binary(int fd, const char *filename);
+void create_file(int fd, const char *filename);
+void delete_file(int fd, const char *filename);
+void write_to_file(int fd, const char *filename, int offset, int n, int data);
+uint32_t get_next_cluster(int fd, uint32_t cluster);
+uint32_t get_file_size(int fd, uint32_t start_cluster);
+void print_help();
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
+    char diskname[128];
+    int fd;
+    unsigned char sector[SECTORSIZE];
+
     if (argc < 3) {
-        fprintf(stderr, "Usage: %s <disk image> <command> [options]\n", argv[0]);
+        print_help();
         return 1;
     }
 
-    int fd = open(argv[1], O_RDONLY);
-    if (fd == -1) {
-        perror("Failed to open disk image");
-        return 1;
+    strcpy(diskname, argv[1]);
+
+    fd = open(diskname, O_SYNC | O_RDWR);
+    if (fd < 0) {
+        printf("could not open disk image\n");
+        exit(1);
     }
 
     if (strcmp(argv[2], "-l") == 0) {
-        list_files(fd);
-    } else if (strcmp(argv[2], "-r") == 0 && argc > 4) {
-        if (strcmp(argv[3], "-a") == 0) {
-            read_file_ascii(fd, argv[4]);
-        } else if (strcmp(argv[3], "-b") == 0) {
-            read_file_binary(fd, argv[4]);
+        list_root_directory(fd);
+    } else if (strcmp(argv[2], "-r") == 0) {
+        if (argc < 5) {
+            print_help();
+            close(fd);
+            return 1;
         }
+        if (strcmp(argv[3], "-a") == 0) {
+            display_file_ascii(fd, argv[4]);
+        } else if (strcmp(argv[3], "-b") == 0) {
+            display_file_binary(fd, argv[4]);
+        } else {
+            print_help();
+        }
+    } else if (strcmp(argv[2], "-c") == 0) {
+        if (argc < 4) {
+            print_help();
+            close(fd);
+            return 1;
+        }
+        create_file(fd, argv[3]);
+    } else if (strcmp(argv[2], "-d") == 0) {
+        if (argc < 4) {
+            print_help();
+            close(fd);
+            return 1;
+        }
+        delete_file(fd, argv[3]);
+    } else if (strcmp(argv[2], "-w") == 0) {
+        if (argc < 7) {
+            print_help();
+            close(fd);
+            return 1;
+        }
+        int offset = atoi(argv[4]);
+        int n = atoi(argv[5]);
+        int data = atoi(argv[6]);
+        write_to_file(fd, argv[3], offset, n, data);
+    } else if (strcmp(argv[2], "-h") == 0) {
+        print_help();
+    } else {
+        print_help();
     }
 
     close(fd);
     return 0;
 }
 
-void list_files(int fd) {
-    unsigned char buffer[CLUSTER_SIZE];
-    lseek(fd, cluster_to_offset(ROOT_DIR_CLUSTER), SEEK_SET);
-    read(fd, buffer, CLUSTER_SIZE);
-
-    for (int i = 0; i < CLUSTER_SIZE / ENTRY_SIZE; i++) {
-        DirectoryEntry *entry = (DirectoryEntry *)(buffer + i * ENTRY_SIZE);
-        if (entry->name[0] == 0x00) break; // End of directory
-        if (entry->name[0] == 0xE5) continue; // Deleted file
-        printf("%s %d\n", entry->name, entry->size);
-    }
+int readsector(int fd, unsigned char *buf, unsigned int snum) {
+    off_t offset;
+    int n;
+    offset = snum * SECTORSIZE;
+    lseek(fd, offset, SEEK_SET);
+    n = read(fd, buf, SECTORSIZE);
+    return (n == SECTORSIZE) ? 0 : -1;
 }
 
-void read_file_ascii(int fd, const char *filename) {
-    DirectoryEntry entry;
-    unsigned char buffer[CLUSTER_SIZE];
-    int found = 0;
+int writesector(int fd, unsigned char *buf, unsigned int snum) {
+    off_t offset;
+    int n;
+    offset = snum * SECTORSIZE;
+    lseek(fd, offset, SEEK_SET);
+    n = write(fd, buf, SECTORSIZE);
+    fsync(fd);
+    return (n == SECTORSIZE) ? 0 : -1;
+}
 
-    lseek(fd, cluster_to_offset(ROOT_DIR_CLUSTER), SEEK_SET);
-    read(fd, &entry, sizeof(entry));
+void list_root_directory(int fd) {
+    unsigned char sector[SECTORSIZE];
+    struct fat_boot_sector *boot;
+    uint32_t root_cluster;
 
-    while (strncmp(entry.name, filename, strlen(filename)) == 0) {
-        found = 1;
-        uint32_t cluster = entry.start;
+    readsector(fd, sector, 0);
+    boot = (struct fat_boot_sector *)sector;
+    root_cluster = boot->fat32.root_cluster;
 
-        while (cluster < EOF_CLUSTER) {
-            lseek(fd, cluster_to_offset(cluster), SEEK_SET);
-            read(fd, buffer, CLUSTER_SIZE);
-            printf("%.*s", entry.size, buffer); // Only print the file size amount of data
-            cluster = read_fat_entry(fd, cluster);
+    unsigned char cluster[CLUSTERSIZE];
+    struct msdos_dir_entry *dep;
+
+    readsector(fd, cluster, root_cluster);
+    dep = (struct msdos_dir_entry *)cluster;
+
+    for (int i = 0; i < CLUSTERSIZE / sizeof(struct msdos_dir_entry); ++i) {
+        if (dep->name[0] == 0x00) break;
+        if (dep->name[0] == 0xE5) {
+            dep++;
+            continue;
         }
+        char name[9];
+        char ext[4];
+        strncpy(name, (char *)dep->name, 8);
+        name[8] = '\0';
+        strncpy(ext, (char *)dep->name + 8, 3);
+        ext[3] = '\0';
 
-        if (found) break;
-    }
-
-    if (!found) {
-        printf("File not found.\n");
-    }
-}
-
-void read_file_binary(int fd, const char *filename) {
-    DirectoryEntry entry;
-    unsigned char buffer[CLUSTER_SIZE];
-    int found = 0;
-
-    lseek(fd, cluster_to_offset(ROOT_DIR_CLUSTER), SEEK_SET);
-    read(fd, &entry, sizeof(entry));
-
-    while (strncmp(entry.name, filename, strlen(filename)) == 0) {
-        found = 1;
-        uint32_t cluster = entry.start;
-
-        while (cluster < EOF_CLUSTER) {
-            lseek(fd, cluster_to_offset(cluster), SEEK_SET);
-            read(fd, buffer, CLUSTER_SIZE);
-            for (int i = 0; i < entry.size; i++) {
-                printf("%02X ", buffer[i]);
-                if ((i + 1) % 16 == 0) printf("\n");
-            }
-            cluster = read_fat_entry(fd, cluster);
-        }
-
-        if (found) break;
-    }
-
-    if (!found) {
-        printf("File not found.\n");
+        printf("%s.%s %d\n", name, ext, dep->size);
+        dep++;
     }
 }
 
-uint32_t read_fat_entry(int fd, uint32_t cluster) {
-    uint32_t entry_offset = FAT_START_CLUSTER * CLUSTER_SIZE + cluster * 4;
-    uint32_t next_cluster;
-
-    lseek(fd, entry_offset, SEEK_SET);
-    read(fd, &next_cluster, sizeof(next_cluster));
-
-    return next_cluster & 0x0FFFFFFF; // Mask the high 4 bits
+void display_file_ascii(int fd, const char *filename) {
+    // Implement the function to display the content of a file in ASCII form
 }
 
-off_t cluster_to_offset(uint32_t cluster) {
-    return (cluster - 2) * CLUSTER_SIZE + (FAT_START_CLUSTER * SECTOR_SIZE);
+void display_file_binary(int fd, const char *filename) {
+    // Implement the function to display the content of a file in binary form
+}
+
+void create_file(int fd, const char *filename) {
+    // Implement the function to create a file
+}
+
+void delete_file(int fd, const char *filename) {
+    // Implement the function to delete a file
+}
+
+void write_to_file(int fd, const char *filename, int offset, int n, int data) {
+    // Implement the function to write data to a file
+}
+
+uint32_t get_next_cluster(int fd, uint32_t cluster) {
+    unsigned char sector[SECTORSIZE];
+    uint32_t fat_offset = cluster * 4;
+    uint32_t sector_number = FAT_START_SECTOR + (fat_offset / SECTORSIZE);
+    uint32_t entry_offset = fat_offset % SECTORSIZE;
+
+    readsector(fd, sector, sector_number);
+    uint32_t *fat_entry = (uint32_t *)(sector + entry_offset);
+    return *fat_entry & 0x0FFFFFFF;
+}
+
+uint32_t get_file_size(int fd, uint32_t start_cluster) {
+    uint32_t cluster = start_cluster;
+    uint32_t size = 0;
+
+    while (cluster < FAT_EOC) {
+        size += CLUSTERSIZE;
+        cluster = get_next_cluster(fd, cluster);
+    }
+
+    return size;
+}
+
+void print_help() {
+    printf("Usage: fatmod DISKIMAGE [option] [arguments]\n");
+    printf("Options:\n");
+    printf("  -l                    List files in the root directory\n");
+    printf("  -r -a FILENAME        Display the content of FILENAME in ASCII form\n");
+    printf("  -r -b FILENAME        Display the content of FILENAME in binary form\n");
+    printf("  -c FILENAME           Create a file named FILENAME in the root directory\n");
+    printf("  -d FILENAME           Delete a file named FILENAME\n");
+    printf("  -w FILENAME OFFSET N DATA\n");
+    printf("                        Write DATA byte N times to FILENAME starting at OFFSET\n");
+    printf("  -h                    Display this help message\n");
 }
